0235af7ddc4a28f69d42504df7659a6490f24aac
Muhammad-Hishamm <72977798+Muhammad-Hishamm@users.noreply.github.com>
2023-02-28 23:18:25 +0200

    feat : Adding Section 3 database section

--- TEKA/projects/views.py
+++ TEKA/projects/views.py
@@ -1,36 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project,Tag
 # Create your views here.
 
-projectList=[
-    {
-        'id':"1",
-        'title':"The first Id",
-        'description':"the Describtion of the first"
-    }
-    ,
-    {
-        'id':"2",
-        'title':"The second Id",
-        'description':"the Describtion of the second"
-    }
-    ,
-    {
-        'id':"3",
-        'title':"The third Id",
-        'description':"the Describtion of the third"
-    }
-    
-]
-
 def projects(request):
-    return render(request,'projects/projects.html',{'list':projectList})
+    projects=Project.objects.all()
+    context={'projects':projects}
+    return render(request,'projects/projects.html',context)
 
 
 
 def project(request,pk):
-    projectObj=None 
-    for i in projectList:
-        if i['id'] == pk:
-            projectObj=i          
-    return render(request,'projects/single-project.html',{'i':projectObj})
+    projectObj = Project.objects.get(id=pk)    
+         
+    return render(request,'projects/single-project.html',{'project':projectObj})
